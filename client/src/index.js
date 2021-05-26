cd95b587508f716058b9169ac5e4099bd59dac16
SiwooPak <siwoo1620@gmail.com>
2021-05-26 23:56:47 +0900

    add: redux 모듈 추가 및 클라이언트 index.js 수정

--- client/src/index.js
+++ client/src/index.js
@@ -3,18 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+// css framework
+import 'antd/dis/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
 
-import 'and/dis/antd.css';
+// store는 객체밖에 처리 못해서 function과 promise를 처리하기 위해 redux-promise와 redux-thunk 모듈 설치
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
+
 
 ReactDOM.render(
-  // <div>Hello, React!</div>,
-  //<React.StrictMode>
-    <App />,
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()}
+  >
+    <App />
+  </Provider>
+    
   //</React.StrictMode>,
-  document.getElementById('root')
-);
+  , document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
+
